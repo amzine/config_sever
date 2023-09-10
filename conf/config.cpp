707be1cb4c158984e058dd21140915397f49975a
Amine Mimouni <amimouni@c4r1p5.1337.ma>
2023-09-10 22:02:40 +0100

    afasf

--- conf/config.cpp
+++ conf/config.cpp
@@ -24,6 +24,22 @@ std::vector<ConfigServer> Config::getServer() const
     return this->_server;
 }
 
+// bool Config::checkfile(filevector file)
+// {
+//     size_t comma;
+//     for ( filevector::iterator i = file.begin() ;i < file.end(); i++)
+//     {
+//         if (i->data() == "server" || i->data() == "{")
+//         {
+            
+//         }
+        
+//         if (comma = i->find(";") == std::string::npos)
+//             return false;
+//     }
+//     return true;
+// }
+
 int Config::parse(const char *filename, confData &data)
 {
     filevector file;
@@ -56,40 +72,40 @@ int Config::parse(const char *filename, confData &data)
             return 1;
         }   
     }
-    std::cout << this->_server[0] << '\n';
+    for (std::vector<ConfigServer>::iterator i = this->_server.begin(); i != this->_server.end(); i++)
+    {
+        std::vector<t_listen> t = i->get_listen();
+        for (size_t it = 0; it < i->get_listen().size(); it++)
+        {
+            for (size_t j = it + 1; j < i->get_listen().size(); ++j)
+            {
+                if (t[it].port == t[j].port) 
+                {
+                    std::cerr << "Error: found duplicate port" << std::endl;
+                    return 1;
+                }
+
+            }
+        }
+        
+    }
     data.server = this->_server;
 
     return 0;
 }
 
-bool Config::getServerforRequest(ConfigServer &ret, t_listen const address, std::string const hostName) const 
-{
-    std::vector<ConfigServer> possibleServers;
-    for (std::vector<ConfigServer>::const_iterator serveriter = this->_server.begin(); serveriter != this->_server.end(); serveriter++)
-    {
-        std::vector<t_listen> listen = serveriter->get_listen();
-        for (std::vector<t_listen>::iterator listniter = listen.begin(); listniter < listen.end() ; listniter++)
-        {
-            if (address.host == (*listniter).host && address.port == (*listniter).port)
-                possibleServers.push_back((*serveriter));
-        }
-    }
-    if (possibleServers.empty())
-        return false;
-    for ( std::vector<ConfigServer>::iterator servreiter = possibleServers.begin(); servreiter != possibleServers.end(); servreiter++)
-    {
-        std::vector<std::string> servernames = servreiter->get_server_name();
-        for (filevector::iterator servernameIter= servernames.begin(); servernameIter !=servernames.end(); servernameIter++)
-        {
-            if (*servernameIter == hostName)
-            {
-                ret = *servreiter;
-                return true;
-            }
-        }
-    }
-    ret = possibleServers[0];
-    return true;
-}
+// RequestConfiga Config::getConfigForRequest(t_listen const adress,std::string const uri, std::string hostname, const std::string &method, Request &request) const
+//     {
+//         ConfigServer server;
+//         std::string locationPath;
 
+//         hostname = hostname.substr(0, hostname.find_last_of(':'));
+//         this->getServerforRequest(server, adress, hostname);
+//         server = server.getlocationforRequest(uri, locationPath);
+//         if (*(--locationPath.end()) == '/')
+//             locationPath.resize(locationPath.size() - 1);
+//         RequestConfiga config(server, request, uri, method, locationPath);
+//         config.setHostPort(adress);
+//         return config;
+//     }
 
